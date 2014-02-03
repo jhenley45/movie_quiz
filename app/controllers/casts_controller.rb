534878f648be4a9821adae9ad1ca5eae2d589311
Jack Henley <jack.d.henley@gmail.com>
2014-02-03 11:23:36 -0500

    Showing correct/incorrect for actors in movies

--- app/controllers/casts_controller.rb
+++ app/controllers/casts_controller.rb
@@ -6,8 +6,13 @@ class CastsController < ApplicationController
   def index
   	if params[:movie].present?
 	  	@movie = params[:movie]
+	  	#find the cast for this movie
 	  	@cast = Cast.find_cast(@movie)
+
+	  	# get the title of the movie to be passed into the movies controller in correct format
 	  	@movie_names = Tmdb::Movie.find(@movie)
+	  	#get the first search result
+	  	@movie_name = @movie_names.first.original_title
  		end
   end
 
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,20 @@ class MoviesController < ApplicationController
   def index
   	if params[:person].present?
   		person = params[:person]
+  		#find all the movies for this person
   		@movies = Movie.find_movie(person)
+  		#clean resulst
+  		@movie = @movies.first[1]
+  		#create array of titles
+  		@titles = title_array(@movie)
+  		#get the movie title from the params
+  		@movie_name = params[:movie_name]
+  		#determine if the user input matches the titles we have
+  		@answer = movie_check(@movie_name, @titles)
+
+
+
+
   	end
 
 	end
@@ -17,5 +30,23 @@ class MoviesController < ApplicationController
   end
 
   def new
+  end
+
+  private
+
+  def movie_check(movie_title, titles)
+  	if titles.include?(movie_title)
+  		"That is correct"
+  	else
+  		"That is incorrect"
+  	end
+  end
+
+  def title_array(movies)
+  	titles = []
+  	movies.each do |movie|
+  		titles << movie["original_title"]
+  	end
+  	titles
   end
 end
