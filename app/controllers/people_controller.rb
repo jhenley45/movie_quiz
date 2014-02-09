10106c4209908c5447d80af36693ff5d3c6ecc9d
Jack Henley <jack.d.henley@gmail.com>
2014-02-09 18:07:30 -0500

    Added error checking for when a user tries to redirect after a round is over.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,34 +7,40 @@ class PeopleController < ApplicationController
   def create
   	person = params[:person]["name"]
   	movie = params[:person]["movie"]
+    #Check to see if the user is trying to replay the round
+    if !session[:answers].nil?
     #check the answer to see if it's already in the session
-    if session[:answers].include?(person)
-      # We got a cheater on our hands...
-      session[:answers] = nil
-      flash['alert'] = "Sorry, but you already entered #{person}. Final score: #{current_user.rounds.last.score}"
-      redirect_to root_path
-    else
-    	# First returns true if the person is present in the DB
-    	if Person.find_by_name(person) == true
-        # Returns true if the person is in the movie that the user put in
-    		if Person.validate_movie(person, movie) == true
-    			update_score
-    			update_level_up
-          session[:answers] << person
-    			redirect_to new_movie_path(:person => person)
-          flash['notice'] = "Correct! #{person} was in #{movie}."
-  	  	else
-          # the person exitst but is not in this movie
+      if session[:answers].include?(person)
+        # We got a cheater on our hands...
+        session[:answers] = nil
+        flash['alert'] = "Sorry, but you already entered #{person}. Final score: #{current_user.rounds.last.score}"
+        redirect_to root_path
+      else
+      	# First returns true if the person is present in the DB
+      	if Person.find_by_name(person) == true
+          # Returns true if the person is in the movie that the user put in
+      		if Person.validate_movie(person, movie) == true
+      			update_score
+      			update_level_up
+            session[:answers] << person
+      			redirect_to new_movie_path(:person => person)
+            flash['notice'] = "Correct! #{person} was in #{movie}."
+    	  	else
+            # the person exitst but is not in this movie
+              session[:answers] = nil
+    	  			flash['alert'] = "Sorry, but #{person} was not in #{movie}. Final score: #{current_user.rounds.last.score}"
+    	  			redirect_to root_path
+      		end
+      	else
+            # The person does not exist in the DB and was therefore not in the move that brought us here
             session[:answers] = nil
-  	  			flash['alert'] = "Sorry, but #{person} was not in #{movie}. Final score: #{current_user.rounds.last.score}"
-  	  			redirect_to root_path
-    		end
-    	else
-          # The person does not exist in the DB and was therefore not in the move that brought us here
-          session[:answers] = nil
-    			flash['alert'] = "Sorry, but #{person} was not in #{movie}. Final score: #{current_user.rounds.last.score}"
-    			redirect_to root_path
-    	end
+      			flash['alert'] = "Sorry, but #{person} was not in #{movie}. Final score: #{current_user.rounds.last.score}"
+      			redirect_to root_path
+      	end
+      end
+    else
+      flash['alert'] = "That round is over, you cheater."
+      redirect_to root_path
     end
   end
 
