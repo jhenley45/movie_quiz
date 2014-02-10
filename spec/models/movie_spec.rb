e5fd4db7b262e1b521ce0003ae925b97e15ddfe1
Jack Henley <jack.d.henley@gmail.com>
2014-02-10 12:42:53 -0500

    Added testing for .check_movie_name

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -8,9 +8,22 @@ describe Movie do
 		it { should validate_presence_of(:populated) }
 	end
 
-
   describe 'associations' do
   	it {should have_many :cast_members}
   	it {should have_many :people}
   end
+
+  describe '.check_movie_name' do
+  	before :each do
+  		@departed = Movie.create!(title: 'The Departed', tmdb_id: 4433, populated: true)
+  	end
+
+  	it 'Should return false if no movie is found' do
+  		expect(Movie.check_movie_name('dfadsgsfg')).to eq false
+  	end
+  	it 'Should return true for a valid movie' do
+  		expect(Movie.check_movie_name('The Departed')).to eq true
+  	end
+  end
+
 end
