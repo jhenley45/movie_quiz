5911c8fb1331604ad18b183856b0a8ff23ed2ffe
Jack Henley <jack.d.henley@gmail.com>
2014-02-12 10:17:03 -0500

    Added specs for find_movie_in_db

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -27,15 +27,28 @@ describe Movie do
   end
 
   describe '.tmdb_movie_lookup' do
+
   	before :each do
   		@departed = Movie.tmdb_movie_lookup('the departed')
   	end
-
   	it 'should return an array when called' do
   		expect(Movie.tmdb_movie_lookup('test')).to be_an_instance_of(Array)
   	end
   	it 'should return the title of a movie within the array' do
   		expect(@departed.first["original_title"]).to eq 'The Departed'
+  	end
+  end
+
+  describe '.find_movie_in_db' do
+  	before :each do
+  		@departed = Movie.create!(title: 'The Departed', tmdb_id: 4433, populated: true)
+  	end
+
+  	it 'should return the movie object when it is found' do
+  		expect(Movie.find_movie_in_db('the departed')).to eq @departed
+  	end
+  	it 'should return false when no movie is found' do
+  		expect(Movie.find_movie_in_db('dfsdas')).to be false
   	end
   end
 
